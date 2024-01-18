e2731e0ebb14556a574ec7f05cf61b32ff06c5a8
safdar211ali <safdar211@hotmail.com>
2024-01-18 16:18:13 +0500

    updated

---
+++ sayaab/dyeing/doctype/shade_process/shade_process.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Tech Ventures and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shade Process', {
+	// refresh: function(frm) {
+
+	// }
+});
