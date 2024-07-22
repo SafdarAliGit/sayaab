73657f2ac3a591f578f21e361f47901c5bd7db47
safdar211ali <safdar211@hotmail.com>
2024-07-22 13:06:50 +0500

    updated

--- sayaab/util.py
+++ sayaab/util.py
@@ -4,17 +4,20 @@ import frappe
 SERIAL_PORT = 'COM4'  # Change this to your specific port
 BAUD_RATE = 9600
 
+
 def read_serial_data():
     try:
-        with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
-            if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8').strip()
-                return line
+        ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').strip()
+            return line
+
     except serial.SerialException as e:
         return f"Serial error: {e}"
     except Exception as e:
         return f"General error: {e}"
     return None
+
 
 @frappe.whitelist()
 def get_serial_data():
@@ -23,4 +26,3 @@ def get_serial_data():
         return {"status": "success", "data": data}
     else:
         return {"status": "error", "message": "No data available or error reading serial port"}
-
