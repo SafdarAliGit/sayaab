081b78e25466ba1e07e3f4cf11449b89b00772e6
safdar211ali <safdar211@hotmail.com>
2024-07-23 13:00:58 +0500

    updated

--- sayaab/public/js/serial_reader.js
+++ sayaab/public/js/serial_reader.js
@@ -1,16 +1,75 @@
+// $(document).ready(function () {
+//     if ('serial' in navigator) {
+//         let port;
+//         let reader;
+//         let textDecoder;
+//         function reverseString(str) {
+//             return str.split('').reverse().join('');
+//         }
+//         async function connectSerial() {
+//             try {
+//                 // Request the port and open a connection
+//                 port = await navigator.serial.requestPort();
+//                 await port.open({baudRate: 9600});
+//
+//                 // Initialize text decoder
+//                 textDecoder = new TextDecoderStream();
+//                 const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+//                 reader = textDecoder.readable.getReader();
+//
+//                 // Start reading data
+//                 readSerialData();
+//             } catch (error) {
+//                 console.log('Error:', error);
+//             }
+//         }
+//
+//         async function readSerialData() {
+//             while (true) {
+//                 try {
+//                     const {value, done} = await reader.read();
+//                     if (done) {
+//                         reader.releaseLock();
+//                         break;
+//                     }
+//                     // Display the data in the input field
+//                     let reversedValue = reverseString(value.trim());
+//                     let floatValue = parseFloat(reversedValue);
+//                     const inputField = $('input[data-fieldname="qty"]');
+//                     if (inputField.length) {
+//                         inputField.val(floatValue);
+//                     }
+//                 } catch (error) {
+//                     console.log('Error reading data:', error);
+//                     break;
+//                 }
+//             }
+//         }
+//
+//         $(document).on('click', '.item-name', function () {
+//             connectSerial();
+//         });
+//     } else {
+//         console.log('Web Serial API is not supported in this browser.');
+//     }
+// });
+
 $(document).ready(function () {
     if ('serial' in navigator) {
         let port;
         let reader;
         let textDecoder;
+        let readTimeout;
+
         function reverseString(str) {
             return str.split('').reverse().join('');
         }
+
         async function connectSerial() {
             try {
                 // Request the port and open a connection
                 port = await navigator.serial.requestPort();
-                await port.open({baudRate: 9600});
+                await port.open({ baudRate: 9600 });
 
                 // Initialize text decoder
                 textDecoder = new TextDecoderStream();
@@ -19,6 +78,9 @@ $(document).ready(function () {
 
                 // Start reading data
                 readSerialData();
+
+                // Set a timeout to close the port after 2 seconds
+                readTimeout = setTimeout(disconnectSerial, 3000);
             } catch (error) {
                 console.log('Error:', error);
             }
@@ -27,7 +89,7 @@ $(document).ready(function () {
         async function readSerialData() {
             while (true) {
                 try {
-                    const {value, done} = await reader.read();
+                    const { value, done } = await reader.read();
                     if (done) {
                         reader.releaseLock();
                         break;
@@ -46,6 +108,23 @@ $(document).ready(function () {
             }
         }
 
+        async function disconnectSerial() {
+            try {
+                if (reader) {
+                    await reader.cancel();
+                    await reader.releaseLock();
+                }
+                if (port) {
+                    await port.close();
+                    port = null;
+                }
+                clearTimeout(readTimeout); // Clear the timeout
+                console.log('Serial port closed after 2 seconds');
+            } catch (error) {
+                console.log('Error closing serial port:', error);
+            }
+        }
+
         $(document).on('click', '.item-name', function () {
             connectSerial();
         });
@@ -53,94 +132,3 @@ $(document).ready(function () {
         console.log('Web Serial API is not supported in this browser.');
     }
 });
-
-// $(document).ready(function () {
-//     if ('serial' in navigator) {
-//         let port;
-//         let reader;
-//         let textDecoder;
-//
-//         function reverseString(str) {
-//             return str.split('').reverse().join('');
-//         }
-//
-//         function debounce(func, timeout = 300) {
-//             let timer;
-//             return (...args) => {
-//                 clearTimeout(timer);
-//                 timer = setTimeout(() => { func.apply(this, args); }, timeout);
-//             };
-//         }
-//
-//         let readings = [];
-//         const MAX_READINGS = 10;
-//
-//         function smoothData(newReading) {
-//             readings.push(newReading);
-//             if (readings.length > MAX_READINGS) {
-//                 readings.shift(); // Remove the oldest reading
-//             }
-//             return readings.reduce((a, b) => a + b, 0) / readings.length; // Average of readings
-//         }
-//
-//         function updateInputField(value) {
-//             const inputField = $('input[data-fieldname="qty"]');
-//             if (inputField.length) {
-//                 inputField.val(value);
-//             }
-//         }
-//
-//         const debouncedUpdate = debounce(updateInputField);
-//
-//         function parseWeightData(data) {
-//             let reversedData = reverseString(data.trim());
-//             let floatValue = parseFloat(reversedData);
-//             return isNaN(floatValue) ? null : floatValue;
-//         }
-//
-//         async function connectSerial() {
-//             try {
-//                 // Request the port and open a connection
-//                 port = await navigator.serial.requestPort();
-//                 await port.open({baudRate: 9600});
-//
-//                 // Initialize text decoder
-//                 textDecoder = new TextDecoderStream();
-//                 const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
-//                 reader = textReader.readable.getReader();
-//
-//                 // Start reading data
-//                 readSerialData();
-//             } catch (error) {
-//                 console.log('Error:', error);
-//             }
-//         }
-//
-//         async function readSerialData() {
-//             while (true) {
-//                 try {
-//                     const {value, done} = await reader.read();
-//                     if (done) {
-//                         reader.releaseLock();
-//                         break;
-//                     }
-//                     // Parse and smooth the data before displaying it
-//                     const parsedValue = parseWeightData(value);
-//                     if (parsedValue !== null) {
-//                         let smoothedValue = smoothData(parsedValue);
-//                         debouncedUpdate(smoothedValue);
-//                     }
-//                 } catch (error) {
-//                     console.log('Error reading data:', error);
-//                     break;
-//                 }
-//             }
-//         }
-//
-//         $(document).on('click', '.item-name', function () {
-//             connectSerial();
-//         });
-//     } else {
-//         console.log('Web Serial API is not supported in this browser.');
-//     }
-// });
